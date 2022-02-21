8696cba1c4cbf7909d4166a474f8e52cdf6af93e
Jon-OP <92676936+Jon-OP@users.noreply.github.com>
2022-02-22 00:35:57 +0800

    Reverted

--- main.cpp
+++ main.cpp
@@ -100,12 +100,12 @@ int main() {
 
         switch (userChoice) {
             case 1:
-                //might need to return a pointer to the movieList
-                adminInventory.inventoryConsoleMenu(adminMovie);
+                adminInventory.inventoryConsoleMenu();
                 break;
             case 2:
-                //accept inventory as parameter || accept pointer to movieList as parameter
-                adminTransaction.transactionMenu(adminMovie);
+                //accept inventory as parameter
+                adminTransaction.transactionMenu();
+                std::cout << "You entered 2\n";
                 break;
             case 3:
                 std::cout << "\n\t>> Goodbye\n\t>> Enter any key to exit:";
